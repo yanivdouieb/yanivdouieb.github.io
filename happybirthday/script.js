f38ab226f72c14dd924a90abe14cd789284e0da4
yanivdouieb <76172599+yanivdouieb@users.noreply.github.com>
2021-05-31 13:42:37 +0200

    Update script.js

--- happybirthday/script.js
+++ happybirthday/script.js
@@ -115,7 +115,7 @@ send.addEventListener("click", () => {
   var uFont = btoa(localStorage.getItem("font"));
   var uName = btoa(localStorage.getItem("name"));
   var uMsg = btoa(localStorage.getItem("msg"));
-  var baseUrl = "yanivdouieb.github.io/happybirthday/happy/";
+  var baseUrl = "/happy/";
   var url = `${baseUrl}?bg=${uBgColor}&emoji=${uEmoji}&font=${uFont}&name=${uName}&msg=${uMsg}`;
   var api = "12abbdbc8a193980157fc64cea4190d1be77c";
   document.getElementsByTagName(
